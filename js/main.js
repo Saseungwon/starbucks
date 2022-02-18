80cbe879d1e413c831f87c0b16a7ebfc42d91101
dfg1425@naver.com <dfg1425@naver.com>
2022-02-18 17:30:17 +0900

    20220218

--- js/main.js
+++ js/main.js
@@ -20,6 +20,21 @@ searchInputEl.addEventListener('blur', function(){    // focus 반대개념 : bl
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', function(){
-  console.log('scroll!');
-});
+window.addEventListener('scroll', _.throttle(function(){
+  // lodash  _.throttle(함수, 시간)
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {  // scroll 위치가 500 보다 크면?
+    // 배지 숨기기
+        //gsap.to(요소, 지속시간, 옵션) - gsap 애니메니션 라이브러리 사용
+        gsap.to(badgeEl, 0.6, {
+          opacity: 0,
+          display: 'none'   // 시각적으로뿐 아니라 실제로도 없어야 해서
+        });
+  } else{
+    // 배지 보이기        
+        gsap.to(badgeEl, 0.6, {
+          opacity: 1,
+          display: 'block' 
+        });
+  }
+}, 300));

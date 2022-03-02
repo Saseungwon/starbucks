2be1dcfcb270c761d9bca0b4f40eb2865dfc0e4f
dfg1425@naver.com <dfg1425@naver.com>
2022-03-02 17:40:53 +0900

    20220302_end

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ searchInputEl.addEventListener('blur', function(){    // focus 반대개념 : bl
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){
   // lodash  _.throttle(함수, 시간)
@@ -30,15 +31,29 @@ window.addEventListener('scroll', _.throttle(function(){
           opacity: 0,
           display: 'none'   // 시각적으로뿐 아니라 실제로도 없어야 해서
         });
+    // 버튼 보이기
+        gsap.to(toTopEl, .2, {
+          x: 0
+        });
   } else{
     // 배지 보이기        
         gsap.to(badgeEl, 0.6, {
           opacity: 1,
           display: 'block' 
         });
+    // 버튼 숨기기!
+        gsap.to(toTopEl, .2, {
+          x: 100
+        });
   }
 }, 300));
 
+// 맨위로
+toTopEl.addEventListener('click', function(){
+  gsap.to(window, .7, {
+    scrollTo: 0   // 0.7초 동안 맨위로 올려준다.
+  });
+});
 
 
 // 음료 사진 fade-in
@@ -149,6 +164,12 @@ spyEls.forEach(function(spyEls){
       triggerElement: spyEls,  // 보여짐 여부를 감시할 요소를 지정
       triggerHook: .8          // 뷰포트 0.8 지점에 걸리면 어떠한 내용이 실행됨
     })          
-    .setClassToggle()
-    .addTo();         // 메소드 체이닝
+    .setClassToggle(spyEls, 'show')
+    .addTo(new ScrollMagic.Controller());         // 메소드 체이닝
 });
+
+
+// FOOTER 연도 출력
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();    //  getFullYear : 2021
+    // textContent : 값을 지정하거나 글자 내용들의 값을 알아내줌
